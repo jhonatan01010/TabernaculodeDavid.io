5ab9bb78d331218f1c7686bcd98fcd25b78fea67
jhonatan01010 <jhontancoro23@gmail.com>
2023-04-19 15:45:24 -0600

    Primer

---
+++ email.php
@@ -0,0 +1,16 @@
+<?php
+  $destinatario
+= 'mecitabernaculodedavid@gmail.com';
+// esto es al correo al que se enviará el mensaje
+  $nombre =$_POST['nombre'];
+  $asunto = $_POST['asunto'];
+  $mensaje = $_POST['mensaje'];
+  $email = $_POST['email'];
+  
+  $header = "Enviado desde la página de AlexCG Design";
+  
+  $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+  
+  mail($destinatario, $asunto, $mensajeCompleto, $header);
+  
+?>
